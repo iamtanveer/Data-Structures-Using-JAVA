73297ee14cb853ee675c88d42f0a82739cf37460
Tanveer Singh <tanveer7537@gmail.com>
2018-01-20 23:31:03 +0530

    Check Armstrong number

--- Fundamentals_2/Check_Armstrong.java
+++ Fundamentals_2/Check_Armstrong.java
@@ -11,6 +11,27 @@ public class Check_Armstrong {
 	
 	public static boolean CheckArmstrong(int n){
 		
-		return true;
+		int length = 0;
+		int temp = n;
+		int temp2 = n;
+		int rem = 0;
+		int sum = 0;
+		
+		while(temp>=1){
+			length++;
+			temp = temp/10;
+		}
+		
+		while(temp2>=1){
+			rem = temp2 % 10;
+			temp2 /= 10;
+			sum = sum + (int) Math.pow(rem, length);
+		}
+		
+		if(sum==n){
+			return true;
+		}else{
+			return false;
+		}
 	}
 }
