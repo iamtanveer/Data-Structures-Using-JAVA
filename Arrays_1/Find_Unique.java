a3769a263f4a8de79b6ac562e1cbabc3dfc6d09d
Tanveer Singh <tanveer7537@gmail.com>
2018-01-24 23:09:17 +0530

    Arrays

---
+++ Arrays_1/Find_Unique.java
@@ -0,0 +1,43 @@
+package Arrays_1;
+
+import java.util.Scanner;
+
+public class Find_Unique {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int arr[] = {2,3,1,6,2,6,3};
+//		System.out.println(iterMethod(arr));
+		System.out.println(bitMethod(arr));
+		
+	}
+	
+	static int bitMethod(int arr[]){
+		int xor = 0;
+		for(int i=0; i<arr.length; i++){
+			xor = xor ^ arr[i];
+		}
+		return xor;
+	}
+	
+	static int iterMethod(int arr[]){
+		int flag = 0;
+		for(int i=0; i<arr.length; i++){
+			for(int j = 0; j<arr.length; j++){
+				if(i!=j){
+					if(arr[i]!=arr[j]){
+						flag = 1;
+					}else{
+						flag = 0;
+						break;
+					}
+				}
+			}
+			
+			if(flag==1){
+				return arr[i];
+			}
+		}
+		
+		return 0;
+	}
+}
