ee24294778fe8b568a6203cad768c46720b7d137
Tanveer Singh <tanveer7537@gmail.com>
2018-02-11 18:26:11 +0530

    Strings

---
+++ Strings/Replace_Char.java
@@ -0,0 +1,23 @@
+package Strings;
+
+public class Replace_Char {
+
+	public static String replaceCharacter(String input, char c1, char c2) {
+		String str = "";
+		
+		for(int i=0; i<input.length(); i++){
+			if(input.charAt(i)==c1){
+				str += c2;
+			}else{
+				str += input.charAt(i);
+			}
+		}
+		
+		return str;
+	}
+	
+	public static void main(String[] args) {
+		System.out.print(replaceCharacter("Tanveer", 'e', 'i'));
+	}
+
+}
