a66acabde98e9bbe45cb5d9486e6508fea09f1d0
Tanveer Singh <tanveer7537@gmail.com>
2018-02-13 14:39:07 +0530

    Strings

---
+++ Strings/Reverse_Each_Word_In_String.java
@@ -0,0 +1,25 @@
+package Strings;
+
+public class Reverse_Each_Word_In_String {
+
+	public static String reverseWords(String input){
+		String output = " ";
+		
+		int index = 0;
+		
+		for(int i = 0; i < input.length(); i++){
+			if(input.charAt(i) == ' '){
+				index = i+1;
+				output += input.charAt(i);
+			}else{
+				output = output.substring(0, index) + input.charAt(i) + output.substring(index);
+			}
+		}
+		return output;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(reverseWords("Hi I am coding"));
+	}
+
+}
